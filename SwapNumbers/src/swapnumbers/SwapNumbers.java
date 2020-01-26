10b6263e42c07af86d7c92d768bfa91e5231197e
TharunSaravanan <bstharun@gmail.com>
2020-01-25 20:20:34 -0600

    Swap Numbers

---
+++ SwapNumbers/src/swapnumbers/SwapNumbers.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swapnumbers;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tharun
+ */
+public class SwapNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a");
+        int a = in.nextInt();
+
+        System.out.println("Enter b");
+        int b = in.nextInt();
+
+        /*int temp = b;
+        
+        b = a;
+        
+        a = temp;
+        */
+        
+        a = a+b;
+        
+        b = a-b;
+        
+        a = a-b;
+              
+        
+        System.out.println("a =" +a + " b =" +b);
+        
+        
+    }
+    
+}
