eeef11effc50206acb5c08bf1dd87851086da5da
TharunSaravanan <bstharun@gmail.com>
2020-05-03 19:07:57 -0500

    Reversing an Array

---
+++ ReverseArray/src/reversearray/ReverseArray.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reversearray;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tharun
+ */
+public class ReverseArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter length of array");
+        int length = in.nextInt();
+
+        int[] numbers = new int[length];
+
+        for (int j = 0; j < numbers.length; j++) {
+            int c = j + 1;
+            System.out.println("Enter item " + c);
+            numbers[j] = in.nextInt();
+        }
+        
+        for (int i = 0; i < numbers.length / 2; i++) {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.length - i - 1];
+            numbers[numbers.length - i - 1] = temp;
+        }
+
+        for (int j = 0; j < numbers.length; j++) {
+            System.out.println(numbers[j]);
+        }
+    }
+
+}
